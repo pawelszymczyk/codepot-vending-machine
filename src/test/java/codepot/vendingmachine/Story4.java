18d6b4c476539f138d8f355e8d047b38361523aa
Pawel Szymczyk <pawel.szymczyk@allegro.pl>
2015-08-24 17:48:20 +0200

    test for story 3

---
+++ src/test/java/codepot/vendingmachine/Story4.java
@@ -0,0 +1,33 @@
+package codepot.vendingmachine;
+
+import codepot.vendingmachine.domain.Coin;
+import codepot.vendingmachine.domain.VendingMachine;
+import codepot.vendingmachine.infrastructure.suncorp.SunCorpProducts;
+import org.junit.Test;
+
+public class Story4 {
+
+    @Test
+    public void shouldFullyIntegrateWithSunCorpProductStorage() {
+        //given
+        VendingMachine vendingMachine = null;
+        /*
+            here integrate VendingMachine with SunCorpProductStorage, try to pick notifiers from container not create them by new eg:
+
+            vendingMachine = new VendingMachine.Builder().withExternalProductStorage(
+                    (JiraServiceNotifier jiraServiceNotifier, MailServiceNotifier mailServiceNotifier) -> {
+                        new SunCorpProductStorage(Lists.newArrayList(jiraServiceNotifier, mailServiceNotifier));
+                    }
+            ).build();
+        */
+
+        vendingMachine.insertCoin(Coin.QUARTER);
+        vendingMachine.insertCoin(Coin.QUARTER);
+
+        //when
+        vendingMachine.selectProduct(SunCorpProducts.CARROT.getCode());
+
+        //then
+        vendingMachine.getProductsTray().contains(SunCorpProducts.CARROT);
+    }
+}

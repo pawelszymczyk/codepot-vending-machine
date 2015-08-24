1534e9848f1c406f1a24557d0d9a9104c316ece9
Pawel Szymczyk <pawel.szymczyk@allegro.pl>
2015-08-24 15:02:10 +0200

    add missing class

---
+++ src/main/java/codepot/vendingmachine/domain/ProductStorage.java
@@ -0,0 +1,16 @@
+package codepot.vendingmachine.domain;
+
+public class ProductStorage {
+
+    public Money getProductPrice(String productCode) {
+        return null;
+    }
+
+    public boolean isProductAvailable(String productCode) {
+        return false;
+    }
+
+    public DefaultProducts getProduct(String productCode) {
+        return null;
+    }
+}

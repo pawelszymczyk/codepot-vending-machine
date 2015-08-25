63bf7e740b8fe38b231b639e0e8e5a23c61666dc
Pawel Szymczyk <pawel.szymczyk@allegro.pl>
2015-08-25 19:35:28 +0200

    fix in suncorp

--- src/main/java/codepot/vendingmachine/infrastructure/suncorp/SunCorpProductStorage.java
+++ src/main/java/codepot/vendingmachine/infrastructure/suncorp/SunCorpProductStorage.java
@@ -1,7 +1,7 @@
 package codepot.vendingmachine.infrastructure.suncorp;
 
-import codepot.vendingmachine.domain.DefaultProducts;
 import codepot.vendingmachine.domain.Money;
+import codepot.vendingmachine.domain.Product;
 import codepot.vendingmachine.domain.ProductNotFoundException;
 import codepot.vendingmachine.infrastructure.notifiers.JiraServiceNotifier;
 import codepot.vendingmachine.infrastructure.notifiers.MailServiceNotifier;
@@ -42,12 +42,12 @@ public class SunCorpProductStorage {
         return true;
     }
 
-    public DefaultProducts getProduct(String productCode) {
+    public Product getProduct(String productCode) {
         return findProductByCode(productCode);
     }
 
-    private DefaultProducts findProductByCode(String productCode) {
-        return Arrays.stream(DefaultProducts.values()).filter(p -> p.getCode().equals(productCode))
+    private Product findProductByCode(String productCode) {
+        return Arrays.stream(SunCorpProducts.values()).filter(p -> p.getCode().equals(productCode))
                 .findAny()
                 .orElseThrow(() -> new ProductNotFoundException(productCode));
     }
